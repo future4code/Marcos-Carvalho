0eaf8f1992dbf2938aedde855a24324c523e464c
RpgMagik <marcossart@live.com>
2021-06-22 16:58:03 -0300

    ok

---
+++ semana2/condicionais/index.js
@@ -0,0 +1,51 @@
+/* const respostaDoUsuario = prompt("Digite o número que você quer testar")
+const numero = Number(respostaDoUsuario)
+
+if (numero % 2 === 0) {
+  console.log("Passou no teste.")
+} else {
+  console.log("Não passou no teste.")
+}
+ //// maior que zero 
+let fruta = prompt("Escolha uma fruta")
+let preco
+switch (fruta) {
+  case "Laranja":
+    preco = 3.5
+    break;
+  case "Maçã":
+    preco = 2.25
+    break;
+  case "Uva":
+    preco = 0.30
+    break;
+  case "Pêra":
+    preco = 5.5
+    break; // BREAK PARA O ITEM c.
+  default:
+    preco = 5
+    break;
+}
+console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
+const numero = Number(prompt("Digite o primeiro número."))
+
+if(numero > 0) {
+  console.log("Esse número passou no teste")
+	let mensagem = "Essa mensagem é secreta!!!"
+}
+
+console.log(mensagem)
+  */////para ver os valores da frutas, o valor da maça 2,25,
+
+/*   const numero = Number(prompt("Digite o primeiro número."))
+
+if(numero > 0) {
+  console.log("Esse número passou no teste")
+	let mensagem = "Essa mensagem é secreta!!!"
+}
+
+console.log(mensagem) Pede para digitar um valor se for maior que zero aparece amensagem que é passei no teste se for menor não passou 
+e se for negativo da erro */
+
+
+
